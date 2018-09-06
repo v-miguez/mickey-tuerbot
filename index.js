f3e2f180cce81209e5cbbe2c78ce80c72b77a992
Victor <v_miguez@outlook.es>
2018-09-06 11:56:16 +0200

    saludo y creators

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ expressApp.post('/secret-path', (req, res)=>{
 	res.send('hola')
 })
 
-bot.command('hola', (ctx)=>ctx.reply('Ey ese'))
+bot.command('hola', (ctx)=>ctx.reply('¡Hola forastero!'))
+bot.command('creators', (ctx)=>ctx.reply('Como veo que eres un humano curioso te contaré algo:\nEn lo más profundo de las alcantarillas se encuentra el reino de los Tres Ratones Ciegos, un lugar próspero con el mejor moho y restos de comida que uno podría imaginar.\n Allí, tres reyes cuidan del reino desde los muros inexpugnables de una caja de Aliexpress: Víctor el Sagaz, Luis el Certero e Inés la Elocuente. Ellos son mis señores y yo les sirvo con este ojo que lo ve todo.'))
+
+// bot.command()
 
 expressApp.listen(3000, () => {
   console.log('Example app listening on port 3000!')
