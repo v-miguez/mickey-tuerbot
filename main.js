ee52857515072e22af5a27a23d5b2751ff5381e7
Victor <v_miguez@outlook.es>
2018-09-06 10:59:55 +0200

    fdfa

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf');
-const bot = new Telegraf(621360089:AAHlBMMYJpypwLmDEWeSehX1mj0xHEf4S2w);
+const bot = new Telegraf("621360089:AAHlBMMYJpypwLmDEWeSehX1mj0xHEf4S2w");
 bot.start((message) => {
   console.log('started:', message.from.id)
   return message.reply('Hello my friend, write anything to start search!!');
