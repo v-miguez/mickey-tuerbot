bf0471339224ff5db970f28cb4cf6338c6c6f5c3
Victor <v_miguez@outlook.es>
2018-09-06 11:32:59 +0200

    ya dice hola

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const expressApp = express()
 
 const bot = new Telegraf("621360089:AAHlBMMYJpypwLmDEWeSehX1mj0xHEf4S2w")
 expressApp.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://bc151c44.ngrok.io')
+bot.telegram.setWebhook('https://9154339f.ngrok.io/secret-path')
 
 expressApp.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+
+expressApp.post('/secret-path', (req, res)=>{
+	console.log('ruta secreta')
+	res.send('hola')
+})
+
+bot.command('hola', (ctx)=>ctx.reply('Ey ese'))
+
 expressApp.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
+
